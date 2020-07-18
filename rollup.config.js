9ec465f7761fdeb3d4996c309464a773d1dc46a4
Patrick Roberts <pat2060@sbcglobal.net>
2020-07-18 14:09:30 -0500

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,53 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import svelte from 'rollup-plugin-svelte';
+import autoPreprocess from 'svelte-preprocess';
+import { terser } from 'rollup-plugin-terser';
+
+import pkg from './package.json';
+
+const production = !process.env.ROLLUP_WATCH;
+const name = pkg.name
+  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+  .replace(/^\w/, m => m.toUpperCase())
+  .replace(/-\w/g, m => m[1].toUpperCase());
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: production,
+      },
+      {
+        file: pkg.main,
+        format: 'umd',
+        name,
+        sourcemap: production,
+      },
+    ],
+    plugins: [
+      resolve({
+        dedupe: [
+          'svelte',
+        ],
+      }),
+      commonjs(),
+      typescript(),
+      svelte({
+        dev: !production,
+        css: css => {
+          css.write('dist/index.css', production);
+        },
+        preprocess: autoPreprocess(),
+      }),
+      production && terser(),
+    ],
+    watch: {
+      clearScreen: false,
+    },
+  },
+];
